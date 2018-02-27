d187f870b572fdb0fabb338dd941445b32a81cde
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2018-02-27 08:22:33 +0100

    New changes

---
+++ js/home.js
@@ -0,0 +1,75 @@
+
+var isRealScroll = true;
+
+$(window).on('scroll', mainNavScrollControl).trigger('scroll');
+$('.m-main-navigation-item').on('click', mainNavClickControl);
+$('.js-contact-us-trigger').on('click', mainNavClickControl);
+
+/* Hover Effect for the Services Boxes Section */
+$('.m-services-box').on('mouseenter', function() {
+	var $this = $(this),
+		serviceBoxSiblings = $this.closest('.js-m-services-box-wrapper').find('.m-services-box').not($this);
+
+	$this.addClass('m-services-box-is-active');
+	serviceBoxSiblings.removeClass('m-services-box-is-active');
+});
+
+function mainNavClickControl(e) {
+	var $this = $(this),
+		targetId = $(this).attr('href');
+
+	$('.m-main-navigation-item').removeClass('is-active-m-main-navigation-item');
+
+	$this.addClass('is-active-m-main-navigation-item');
+
+	isRealScroll = false;
+	$('html, body').animate({
+		scrollTop: $(targetId).position().top
+	}, {
+		duration: 1000,
+		easing: 'swing',
+		complete: function() {
+			isRealScroll = true
+		}
+	});
+
+
+	e.preventDefault();
+}
+
+function mainNavScrollControl() {
+	headerStyleScrollChange();
+	navigationItemsScrollSet();
+}
+
+function headerStyleScrollChange() {
+	// changing the height and the opacity of the header
+	
+		if ( $(this).scrollTop() > 70 && !$('.l-main-header').hasClass('is-mobile-layout') ) {
+			$('.l-main-header-top').slideUp(300);
+			$('.l-main-header').addClass('l-main-header-is-scrolled')
+		} else {
+			$('.l-main-header-top').slideDown(300);
+			$('.l-main-header').removeClass('l-main-header-is-scrolled')
+		}		
+	
+}
+
+function navigationItemsScrollSet() {
+    var scrollPos = $(document).scrollTop();
+	
+	// Setting the active navigation item on scroll event - but only if it's the 'real' scroll event and not triggered by click
+	if ( isRealScroll && !$('.l-main-header').hasClass('is-mobile-layout') ) {
+	    $('.m-main-navigation-item').each(function () {
+	        var currLink = $(this);
+	        var refElement = $('#' + currLink.data('section-id'));
+	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	            $('.m-main-navigation-item').removeClass('is-active-m-main-navigation-item');
+	            currLink.addClass('is-active-m-main-navigation-item');
+	        }
+	        else{
+	            currLink.removeClass('is-active-m-main-navigation-item');
+	        }
+	    });		
+	}
+}
--- js/master.js
+++ js/master.js
@@ -1,10 +1,53 @@
-function mainNavControl() {
-	var targetId = $(this).data('section-id');
-	console.log(targetId);
-}
+$(window).on('resize', function() {
+	if ( $('.m-hamburger-menu').is(':visible') ) {
+		$('.l-main-header').addClass('is-mobile-layout');
+	} else {
+		$('.l-main-header').removeClass('is-mobile-layout');
+	}
+}).trigger('resize');
 
-$('.m-main-navigation li').on('click', mainNavControl);
+$('.m-hamburger-menu').on('click', function() {
+	var mainNavigation = $('.m-main-navigation'),
+		hamburgerMenuBtn = $(this);
 
-$('.m-main-navigation li a').on('click', function(e) {
-	e.preventDefault();
+	if ( !mainNavigation.hasClass('is-scrolled-in') ) {
+		mainNavigation.addClass('is-scrolled-in');
+		hamburgerMenuBtn.addClass('is-in-x-mode');
+		mainNavigation.after('<div class="test-overlay"></div>')
+		// $('html, body').addClass('has-no-scroll');
+		$('.test-overlay').animate({
+			opacity: 0.8
+		});
+
+		$('.test-overlay').on('click', function() {
+			mainNavigation.removeClass('is-scrolled-in');
+			hamburgerMenuBtn.removeClass('is-in-x-mode');
+			// $('html, body').removeClass('has-no-scroll');
+			$('.test-overlay').fadeOut(300, function() {
+				$(this).remove();
+			});
+		})
+	} else {
+		mainNavigation.removeClass('is-scrolled-in');
+		hamburgerMenuBtn.removeClass('is-in-x-mode');
+		// $('html, body').removeClass('has-no-scroll');
+		$('.test-overlay').fadeOut(300, function() {
+			$(this).remove();
+		});
+	}
 });
+
+$('.js-job-post-hidden-toggle').on('click', function(e) {
+	var $this = $(this),
+		hiddenText = $this.closest('.m-job-posts_item').find('.m-job-posts_hidden-text');
+
+	if ( hiddenText.is(':visible') ) {
+		$this.text('Read more');
+		hiddenText.slideUp(300);
+	} else {
+		$this.text('Read less');
+		hiddenText.slideDown(300);
+	}
+
+	e.preventDefault();
+})
